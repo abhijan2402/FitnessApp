0f4ccbf2c5cfb90c09e4e160e0c11f2d2b64fe51
aayutha <thatheraaayushi@gmail.com>
2023-06-20 20:52:40 +0530

    Account

--- App.jsx
+++ App.jsx
@@ -2,15 +2,10 @@ import {
   StyleSheet, View,
 } from 'react-native';
 import WaterDrink from './src/screens/dashboard/WaterDrink';
-import Account from './src/screens/profile/Account';
-import CreateProfile from './src/screens/profile/CreateProfile';
-import Notification from './src/screens/dashboard/Notification';
-import Profile from './src/screens/dashboard/Profile';
 function App() {
   return (
     <View style={{ flex: 1 }}>
-      <Account />
-      {/* <Notification/> */}
+      <WaterDrink/>
     </View>
   );
 }
