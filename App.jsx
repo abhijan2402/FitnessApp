3e36ed058add12b2e5e1d08a810d755ebdc2c651
abhijan2402 <abhishek.jangid643@gmail.com>
2023-06-21 14:22:54 +0530

    Adding Meal Planner page

--- App.jsx
+++ App.jsx
@@ -3,10 +3,12 @@ import {
 } from 'react-native';
 import Dashboard from './src/screens/dashboard/Dashboard';
 import ActivityTracker from './src/screens/dashboard/ActivityTracker';
+import MealHome from './src/screens/mealplanner/MealHome';
 function App() {
   return (
     <View style={{ flex: 1 }}>
-      <ActivityTracker/>
+      {/* <ActivityTracker/> */}
+      <MealHome />
     </View>
   );
 }
