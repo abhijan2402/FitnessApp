ffa1747448ed0a585b00bd11dc6d96361359f59b
aayutha <thatheraaayushi@gmail.com>
2023-07-14 14:19:16 +0530

    Add Schedule

--- App.jsx
+++ App.jsx
@@ -8,19 +8,20 @@ import Reminders from './src/screens/Sleep/Reminders';
 import WorkoutTracker from './src/components/workout/WorkoutTracker';
 import FullBodyWorkout from './src/components/workout/FullBodyWorkout';
 import WorkoutDetails from './src/components/workout/WorkoutDetails';
-import Dashboard from './src/screens/dashboard/Dashboard';
 import BottomTab from './src/Navigators/BottomTab'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ActivityTracker from './src/screens/dashboard/ActivityTracker';
+// import ActivityTracker from './src/screens/dashboard/ActivityTracker';
 import WorkOutHome from './src/screens/workoutTracker/WorkOutHome';
 import WorkOutInfo from './src/screens/workoutTracker/WordOutInfo';
+import AddSchedule from './src/screens/workoutTracker/AddSchedule';
 function App() {
   const Stack = createNativeStackNavigator();
   return (
     <View style={{ flex: 1 }}>
       {/* <ActivityTracker /> */}
       <WorkOutInfo />
+     
     </View>
     //   <NavigationContainer>
     //   <Stack.Navigator initialRouteName='Bottomtab' screenOptions={{
