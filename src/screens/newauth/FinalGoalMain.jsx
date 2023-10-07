83302bdb3d88cd6712c9df4c0af1fa4a88c5fd62
abhijan2401 <abhishek.j056@gmail.com>
2023-10-07 14:29:34 +0530

    Adding

--- src/screens/newauth/FinalGoalMain.jsx
+++ src/screens/newauth/FinalGoalMain.jsx
@@ -2,11 +2,12 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { ImageBackground } from 'react-native'
 import { TouchableOpacity } from 'react-native'
+import { SCREENS } from '../../constants/Screens'
 
 const FinalGoalMain = ({ navigation }) => {
     return (
         <ImageBackground source={require('../../../assets/images/FInalImage.png')} style={{ width: "100%", height: "100%" }}>
-            <TouchableOpacity style={styles.MainTxt} onPress={() => { navigation.navigate(SCREENS.FINALGOALMAIN) }}>
+            <TouchableOpacity style={styles.MainTxt} onPress={() => { navigation.navigate(SCREENS.LOGIN) }}>
                 <Text style={{ color: "#7265E3", fontSize: 16, fontWeight: "500" }}>Get Statrted!</Text>
             </TouchableOpacity>
         </ImageBackground>
