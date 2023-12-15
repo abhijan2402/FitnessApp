67b1e63391d32ac7e390e423ca81710c2cd6e877
jayesh <jayeshukalkar@gmail.com>
2023-12-15 21:34:21 +0530

    freelance payment pending

--- src/constants/Fonts.js
+++
@@ -1,6 +0,0 @@
-export const FONTS = {
-    FONT_POPPINS_BOLD:'Poppins-Bold',
-    FONT_POPPINS_MEDIUM:'Poppins-Medium',
-    FONT_POPPINS_REGULAR : 'Poppins-Regular',
-    FONT_POPPINS_SEMIBOLD : 'Poppins-SemiBold'
-}
